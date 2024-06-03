a574b7658f9d188c6700458d2db429a78fad003c
tntmancer <tntmancer@gmail.com>
2024-06-03 13:09:29 -0400

    Updated packages to enable using import

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-const express = require('express') // Equivalent to import
+import express from 'express';
 const app = express()
-app.get('/hello', (req, res) => {res.send('Hello World!')})
+app.get('/hello', (req, res) => {res.send('Life is good!')})
+app.get('/', (req, res) => {
+  res.send('Welcome to Full Stack Development!')})
 app.listen(4000)

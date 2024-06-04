ef4ea5ae2abb50d68f3def437a900a8cbffad0ba
tntmancer <tntmancer@gmail.com>
2024-06-04 11:42:39 -0400

    a5 done I believe

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import express from "express";
 import Lab5 from "./Lab5/index.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import AssignmentsRoutes from "./Kanbas/Assignments/routes.js";
 import cors from "cors";
 const app = express();
-app.use(cors());                    // make sure cors is used right after creating the app
-app.use(express.json());
-Lab5(app);                          // express instance
-app.listen(process.env.PORT || 4000);
+app.use(cors());
+app.use(express.json()); // do all your work after this line
+ModuleRoutes(app);
+CourseRoutes(app);
+AssignmentsRoutes(app);
+Lab5(app);
+app.listen(4000);
